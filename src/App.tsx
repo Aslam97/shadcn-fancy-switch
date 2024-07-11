bd732a3a2bac6408a4e10f62a8504cd3c092069c
Aslam H <iupin5212@gmail.com>
2024-07-11 14:59:13 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+import { FancySwitch } from '@/components/fancy-switch/fancy-switch'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { cn } from './lib/utils'
+
+const options: string[] = ['Delivery', 'Pickup', 'Shipping']
+
+const FormSchema = z.object({
+  option: z.string().min(1, {
+    message: 'Option is required'
+  })
+})
+
+function App() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      option: options[0]
+    }
+  })
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    console.log(data)
+  }
+
+  return (
+    <div className="flex min-h-screen place-items-center justify-center bg-white p-4">
+      <div className="mx-auto max-w-4xl">
+        <div className="text-center">
+          <h1 className="text-xl font-bold tracking-tight sm:text-3xl">
+            Fancy Switch
+          </h1>
+          <div className="mt-4 flex justify-center">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              <a
+                href="https://github.com/Aslam97/shadcn-tiptap"
+                className="font-semibold text-primary"
+              >
+                View on Github <span aria-hidden="true">→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mt-6 space-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="option"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="sr-only">Username</FormLabel>
+                  <FormControl>
+                    <FancySwitch
+                      {...field}
+                      options={options}
+                      className={cn(
+                        'w-full',
+                        form.formState.errors.option &&
+                          'rounded-full border border-destructive'
+                      )}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/fancy-switch/fancy-switch.tsx
@@ -0,0 +1,100 @@
+import * as React from 'react'
+import { cn } from '@/lib/utils'
+
+export interface FancySwitchProps
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> {
+  value?: string | null
+  onChange?: (value: string) => void
+  options: string[]
+}
+
+export type OptionRefs = {
+  [key: string]: HTMLDivElement | null
+}
+
+const FancySwitch = React.forwardRef<HTMLDivElement, FancySwitchProps>(
+  ({ options, value, onChange, className, ...props }, ref) => {
+    const [selectedOption, setSelectedOption] = React.useState(
+      value ?? options[0]
+    )
+    const [highlighterStyle, setHighlighterStyle] = React.useState({
+      height: 0,
+      width: 0,
+      transform: 'translateX(0)'
+    })
+    const containerRef = React.useRef<HTMLDivElement>(null)
+    const optionRefs = React.useRef<OptionRefs>({})
+
+    React.useImperativeHandle(ref, () => containerRef.current as HTMLDivElement)
+
+    const updateToggle = React.useCallback(() => {
+      const selectedElement = optionRefs.current[selectedOption]
+
+      if (selectedElement && containerRef.current) {
+        const computedStyle = window.getComputedStyle(selectedElement)
+        const paddingLeft = parseInt(
+          computedStyle.getPropertyValue('padding-left')
+        )
+
+        const selectedElementPosition = selectedElement.getBoundingClientRect()
+        const containerPosition = containerRef.current.getBoundingClientRect()
+
+        setHighlighterStyle({
+          height: selectedElementPosition.height,
+          width: selectedElementPosition.width,
+
+          // or just use left-0
+          transform:
+            'translateX(' +
+            (selectedElementPosition.left -
+              containerPosition.left -
+              paddingLeft / 2) +
+            'px)'
+        })
+      }
+    }, [selectedOption])
+
+    React.useEffect(() => {
+      updateToggle()
+    }, [selectedOption, updateToggle])
+
+    const handleOptionChange = (option: string) => {
+      setSelectedOption(option)
+      if (onChange) onChange(option)
+    }
+
+    return (
+      <div className={cn(className)} ref={containerRef} {...props}>
+        <div className="relative inline-flex rounded-full bg-muted p-2">
+          <div
+            className="absolute rounded-full bg-primary transition-all duration-300"
+            style={highlighterStyle}
+          />
+          {options.map((option) => (
+            <div
+              role="radio"
+              aria-checked={selectedOption === option}
+              aria-label={option}
+              tabIndex={0}
+              key={option}
+              ref={(el) => {
+                if (el) optionRefs.current[option] = el
+              }}
+              onClick={() => handleOptionChange(option)}
+              className={cn(
+                'relative flex h-9 cursor-pointer items-center rounded-full px-3.5 text-sm font-medium transition-colors duration-200',
+                { 'text-primary-foreground': selectedOption === option }
+              )}
+            >
+              {option}
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+)
+
+FancySwitch.displayName = 'FancySwitch'
+
+export { FancySwitch }
